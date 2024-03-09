7f1fef3522c818095d7bbf80529b7e623cf62865
brepp22 <brepp@hotmail.com>
2024-03-08 17:12:48 -0800

    project updates

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useState , useEffect } from 'react'
+import axios from 'axios'
+import Figure from './Figure'
+import Card from './Card'
+import DatePicker from './DatePicker'
+const api_key = 'DEMO_KEY'
+const BASE_URL = `https://api.nasa.gov/planetary/apod?api_key=${api_key}`
 
 function App() {
+  const[apod , setApod] = useState()
+useEffect(() => {
+  function getPhoto() {
+    axios.get(BASE_URL)
+      .then(res => {
+         //console.log(res.data)
+         setApod(res.data)
+      })
+      .catch(err => {
+        console.error(err.message)
+      })
+  }
+  getPhoto()
+} , [])
+  if(!apod) return 'Fetching Photo of the Day...'
   return (
-    <p>
-      Read through the instructions in the README.md file to build your NASA
-      app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-    </p>
+     <section> 
+        <Card 
+          title = {apod.title}
+          text = {apod.explanation}
+          imageURL = {apod.url}
+          date = {apod.date}
+        />
+     </section>
   )
 }
 
